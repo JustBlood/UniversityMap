8b743c58c4616ce2ce80a9ac6c127a4a68f83d20
Just <msg.msgq@mail.ru>
2023-03-22 22:31:53 +0300

    edit PanoramaController and views and add Panorama js scripts for work

--- UniversityMap/Controllers/PanoramaController.cs
+++ UniversityMap/Controllers/PanoramaController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using UniversityMap.Data;
+using UniversityMap.Models;
 
 namespace UniversityMap.Controllers
 {
@@ -13,11 +16,37 @@ namespace UniversityMap.Controllers
         }
         public IActionResult Index(string? tag)
         {
+            Panorama panorama;
+
             if (tag == null)
-                return View(_context.Panoramas.FirstOrDefault());
-            
-            var panorama = _context.Panoramas.SingleOrDefault(x => x.Tag == tag);
+                panorama = _context.Panoramas.FirstOrDefault();
+            else
+                panorama = _context.Panoramas.SingleOrDefault(x => x.Tag == tag);
+
+            if (panorama != null)
+            {
+                var PanoramaOptionsScript = CreatePanoramaOptions(panorama.Id);
+                ViewData["PanoramaOptionsScript"] = PanoramaOptionsScript;
+            }
+            // Сохраняет временный файлик, изменяющий ссылку на изображение функцией CreatePanoramaOptions
+            //System.IO.File.Delete("wwwroot/js/panorama/panorama-options-temp.js");
+            //System.IO.File.WriteAllText("wwwroot/js/panorama/panorama-options-temp.js", PanoramaOptionsScript);
+
             return View(panorama);
+        }
+
+        public FileContentResult Show(int? id)
+        {
+            var panorama = _context.Panoramas.FirstOrDefault(p => p.Id == id);
+            if (panorama != null)
+                return File(panorama.Image, "image/jpg");
+            return null;
+        }
+
+        private string CreatePanoramaOptions(int id)
+        {
+            var script = System.IO.File.ReadAllText("wwwroot/js/panorama/panorama-options.js");
+            return script.Replace("+", Url.Action("show", new { id }));
         }
     }
 }
