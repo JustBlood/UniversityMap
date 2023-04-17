29d9383c86968e44275063534a24e4329bfaf797
Maksim <yarovoy.mr@yandex.ru>
2023-04-17 16:32:02 +0300

    changed labels on CFU map

--- UniversityMap/wwwroot/js/wraplink.js
+++ UniversityMap/wwwroot/js/wraplink.js
@@ -2,7 +2,9 @@ window.addEventListener('DOMContentLoaded', function() {
    var path = document.querySelectorAll("path");
    for(let i = 0; i < path.length; i++)
    {
-    let id = path[i].getAttribute("data-code")
-    path[i].setAttribute("onclick", "window.location.href = '/panorama?tag=" + id + "'")
+       let id = path[i].getAttribute("data-code")
+       if (id[0] == 'p') {
+           path[i].setAttribute("onclick", "window.location.href = '/panorama?tag=" + id + "'")
+       }
    }
  });
